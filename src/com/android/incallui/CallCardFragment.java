03dd53032540abd6b7e226afa076a4ad0da1a91d
Haibo Gu <ghbhaha@users.noreply.github.com>
2015-11-19 17:59:13 +0800

    fix

--- src/com/android/incallui/CallCardFragment.java
+++ src/com/android/incallui/CallCardFragment.java
@@ -560,7 +560,7 @@ public class CallCardFragment extends BaseFragment<CallCardPresenter, CallCardPr
         setPrimaryLabel(label);
 
         if (SudaUtils.isSupportLanguage(true) && !TextUtils.isEmpty(number)
-                   && TextUtils.isEmpty(name)) {
+                   && nameIsNumber) {
             mPu.getOnlineNumberInfo(number, new CallBack() {
                     public void execute(String response) {
                          setPrimaryLabel(response);
